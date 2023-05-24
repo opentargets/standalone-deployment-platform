ae116c6da214f9d4f859aab4d674530e3808d3fa
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-05-24 21:42:38 +0100

    compute Makefile external environment

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Iterate over all variables in config_profile.sh that start with OTOPS and 'echo' the variable name and value
+for var in $(env | grep ^OTOPS_PROFILE | cut -d= -f1); do
+    echo "${var}:=${!var}"
+    echo "export ${var}"
+done
