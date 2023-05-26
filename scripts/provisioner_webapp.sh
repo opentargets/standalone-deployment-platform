3777150ce8225c34104677bc5403455116f2ed0e
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-05-26 14:29:26 +0100

    inject devops context

--- scripts/provisioner_webapp.sh
+++ scripts/provisioner_webapp.sh
@@ -13,6 +13,13 @@ source "${SCRIPTDIR}/config.sh"
 #otops_provisioner_webapp_github_release_artifact="bundle-platform.tgz"
 #otops_provisioner_webapp_github_release_url="${otops_provisioner_webapp_github_release_url_base}/v${otops_provisioner_webapp_version}/${otops_provisioner_webapp_github_release_artifact}"
 otops_provisioner_webapp_path_static_data_context="${otops_deployment_path_webapp_root}/data"
+otops_provisioner_webapp_path_devops_context="${otops_deployment_path_webapp_root}/config.js"
+otops_provisioner_webapp_path_devops_context_template="${otops_deployment_path_webapp_root}/config.template"
+# Web Application DevOps Context - THIS SHOULD BE INJECTED FROM THE ENVIRONMENT, because it contains information that depends on the 'docker-compose' file
+export DEVOPS_CONTEXT_PLATFORM_APP_CONFIG_URL_API="'http:\/\/localhost:8090\/api\/v4\/graphql'"
+export DEVOPS_CONTEXT_PLATFORM_APP_CONFIG_URL_API_BETA="'http:\/\/localhost:8090\/api\/v4\/graphql'"
+export DEVOPS_CONTEXT_PLATFORM_APP_CONFIG_EFO_URL="'\/data\/ontology\/efo_json\/diseases_efo.jsonl'"
+export DEVOPS_CONTEXT_PLATFORM_APP_CONFIG_GOOGLE_TAG_MANAGER_ID="'GTM-WPXWRDV'"
 
 # Main
 logi "Provisioning Web App"
@@ -40,4 +47,21 @@ tar -xzf "${otops_path_webapp_source_bundle}" -C "${otops_deployment_path_webapp
 # Attach the web app static context under 'data' folder in the web app deployment folder
 logi "Attaching Web App static context under 'data' folder in the Web App deployment folder '${otops_deployment_path_webapp_root}'"
 cp -r "${otops_deployment_path_webapp_static_data_context}/*" "${otops_provisioner_webapp_path_static_data_context}"
+# TODO Environment configuration injection
+# Copy the devops context template into the devops context
+logi "Copying Web App devops context template '${otops_provisioner_webapp_path_devops_context_template}' into Web App devops context '${otops_provisioner_webapp_path_devops_context}'"
+cp "${otops_provisioner_webapp_path_devops_context_template}" "${otops_provisioner_webapp_path_devops_context}"
+for envvar in $(cat ${otops_provisioner_webapp_path_devops_context} | egrep -o "DEVOPS[_A-Z]+$"); do
+    export key=${envvar}
+    export value=${!envvar:-undefined}
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo -e "\t[CONTEXT] Injecting '${key}=${value}'"
+        sed -r -i "s/${key}(\W|$)/${value};/g" ${otops_provisioner_webapp_path_devops_context}
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo -e "\t[CONTEXT] Injecting '${key}=${value}'"
+        sed -E -i ".bak" "s/${key}(\W|$)/${value};/g" ${otops_provisioner_webapp_path_devops_context}
+    else
+        echo "Unsupported OS: please use Mac or Linux"
+    fi
+done
 logi "[Web App Provisioner] Done"
