8e4aba8bf7f01457ec8c968d6e876227d1c271e6
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-05-26 12:29:17 +0100

    wire deployment sublocations from Makefile

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,19 +7,19 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Environment
 # Clickhouse
 export otops_path_clickhouse_source_data_volume="${OTOPS_PATH_RELEASE}/clickhouse.tgz"
-export otops_deployment_path_clickhouse_volume="${OTOPS_PATH_DEPLOYMENT}/clickhouse"
+export otops_deployment_path_clickhouse_volume="${OTOPS_PATH_DEPLOYMENT_CLICKHOUSE}"
 export otops_deployment_path_clickhouse_data="${otops_path_clickhouse_volume}/data"
 export otops_deployment_path_clickhouse_config="${otops_path_clickhouse_volume}/config.d"
 export otops_deployment_path_clickhouse_users="${otops_path_clickhouse_volume}/users.d"
 # Elastic Search
 export otops_path_elastic_search_source_data_volume="${OTOPS_PATH_RELEASE}/elastic_search.tgz"
-export otops_deployment_path_elastic_search_volume="${OTOPS_PATH_DEPLOYMENT}/elasticsearch"
+export otops_deployment_path_elastic_search_volume="${OTOPS_PATH_DEPLOYMENT_ELASTIC_SEARCH}"
 export otops_deployment_path_elastic_search_data="${otops_path_elastic_search_volume}/data"
 export otops_docker_elastic_search_volume="esdata"
 # Web App
 export otops_path_webapp_source_bundle="${OTOPS_PATH_RELEASE}/webapp.tgz"
 export otops_path_webapp_source_static_data_context="${OTOPS_PATH_RELEASE}/webapp_static_context"
-export otops_deployment_path_webapp_root="${OTOPS_PATH_DEPLOYMENT}/webapp"
+export otops_deployment_path_webapp_root="${OTOPS_PATH_DEPLOYMENT_WEBAPP}"
 
 # Helper functions
 # Logging function with log levels
