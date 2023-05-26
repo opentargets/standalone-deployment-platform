90d0a67f721cedfdb7b56e38e8c18f8978184997
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-05-26 10:58:55 +0100

    Clickhouse provisioner

---
+++ scripts/provisioner_clickhouse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Clickhouse Provisioner
+
+# Bootstrap
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${SCRIPTDIR}/config.sh"
+
+# Main
+logi "Provisioning Clickhouse"
+# Check if the clickhouse data volume exists
+if [ -d "${otops_deployment_path_clickhouse_volume}" ]; then
+    logw "WIPING OUT Existing Clickhouse data volume at '${otops_deployment_path_clickhouse_volume}'"
+    rm -rf "${otops_deployment_path_clickhouse_volume}/*"
+fi
+
+# Check create clickhouse data volume deployment folder
+if [ ! -d "${otops_deployment_path_clickhouse_volume}" ]; then
+    logi "Creating Clickhouse data volume deployment folder at '${otops_deployment_path_clickhouse_volume}'"
+    mkdir -p "${otops_deployment_path_clickhouse_volume}"
+fi
+
+# Uncompress clickhouse source data volume tarball into clickhouse data volume deployment folder
+logi "Uncompressing Clickhouse source data volume tarball '${otops_path_clickhouse_source_data_volume}' into Clickhouse data volume deployment folder '${otops_deployment_path_clickhouse_volume}'"
+tar -xzf "${otops_path_clickhouse_source_data_volume}" -C "${otops_deployment_path_clickhouse_volume}"
+logi "[Clickhouse Provisioner] Done"
